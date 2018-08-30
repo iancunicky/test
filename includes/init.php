4f9bb6ebb299f6ca1698e673637571539b728c9b
iancunicky <nicolae.iancu.dev@gmail.com>
2018-08-30 16:30:02 +0300

    first commit

---
+++ includes/init.php
@@ -0,0 +1,10 @@
+<?php
+spl_autoload_register('my_autoload_class');
+
+
+function my_autoload_class($my_class){
+
+
+
+}
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+	<head>
+		<title>New one yeah!</title>
+	</head>
+	<body>
+		<?php include_once('includes/init.php');?>
+	</body>
+</html>
